871be62e04e3ae439665600e46311229108ba41c
suparansharma <suparansharmap@gmail.com>
2022-05-21 23:59:27 +0600

    all about out btn

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../images/logo.png"
 import "./Header.css"
+import { UserContext } from './../../App';
 
 const Header = () => {
+  const [loggedInUser,setLoggedInUser] = useContext(UserContext);
   return <div className="header">
       <img src={logo} alt="" srcset="" />
       <nav>
           <Link to="/shop">Shop</Link>
           <Link to="/review">Order Review</Link>
           <Link to="/inventor">Manage Inventory</Link>
-          <button>Sign out</button>
+          <button onClick={()=>setLoggedInUser({})}>Sign out</button>
           </nav>
   </div>;
 };
